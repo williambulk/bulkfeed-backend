654320b1244ad3ee5b0c8c3e5b555c38c2c3fbba
William Kunitake <william.kunitake@gmail.com>
2025-02-01 15:23:22 -0800

    Setup TypeScript on Node

---
+++ dist/server.js
@@ -0,0 +1,15 @@
+import "dotenv/config";
+import express from "express";
+const app = express();
+// Setting up the port
+const PORT = process.env.PORT;
+// Setting up the JSON middleware
+app.use(express.json());
+// Basic route
+app.get("/", (req, res) => {
+    res.send("Testing the BulkFeed API");
+});
+// Starting server, listening to the port set in the .env file
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
