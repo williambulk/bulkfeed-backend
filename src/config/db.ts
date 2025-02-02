40bb235db4f7936c6c033e0c36fe7f4260032551
William Kunitake <william.kunitake@gmail.com>
2025-02-02 14:10:28 -0800

    Basic database file

---
+++ src/config/db.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const mongoConnect = async () => {
+  try {
+    await mongoose.connect(process.env.STRING_DB as string);
+    console.log("Connection to the database successful.");
+  } catch (error) {
+    console.error("Failed to connect to the database.", error);
+    process.exit(1);
+  }
+};
+
+export default mongoConnect;
