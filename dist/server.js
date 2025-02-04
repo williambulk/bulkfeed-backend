ca251d7344fb6c355d8c045d1561d7a0c34e68f5
William Kunitake <william@bulkdesign.com.br>
2025-02-03 22:18:24 -0800

    Setup CORS and added AI News Routes

--- dist/server.js
+++ dist/server.js
@@ -1,14 +1,20 @@
 import "dotenv/config";
+import cors from "cors";
 import express from "express";
+import AiNewsRoutes from "./routes/ai-news-routes.js";
 const app = express();
-// Setting up the port
-const PORT = process.env.PORT;
+// Setting up CORS
+const corsOptions = {
+    origin: "http://localhost:2000",
+    optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 // Setting up the JSON middleware
 app.use(express.json());
-// Basic route
-app.get("/", (req, res) => {
-    res.send("Testing the BulkFeed API");
-});
+// Setting up the routes
+app.use("/ai-news", AiNewsRoutes);
+// Setting up the port
+const PORT = process.env.PORT;
 // Starting server, listening to the port set in the .env file
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
