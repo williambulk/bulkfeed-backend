19348c6b680c684c7ab7e1a9240f424055f9cf2d
William Kunitake <william@bulkdesign.com.br>
2025-02-04 00:33:30 -0800

    Compiled server.js

--- dist/server.js
+++ dist/server.js
@@ -1,7 +1,8 @@
 import "dotenv/config";
 import cors from "cors";
 import express from "express";
-import AiNewsRoutes from "./routes/ai-news-routes.js";
+import AiNewsRouter from "./routes/ai-news-routes.js";
+import mongoConnect from "./config/db.js";
 const app = express();
 // Setting up CORS
 const corsOptions = {
@@ -11,8 +12,10 @@ const corsOptions = {
 app.use(cors(corsOptions));
 // Setting up the JSON middleware
 app.use(express.json());
+// Calling MongoDB
+mongoConnect();
 // Setting up the routes
-app.use("/ai-news", AiNewsRoutes);
+app.use("/ai-news", AiNewsRouter);
 // Setting up the port
 const PORT = process.env.PORT;
 // Starting server, listening to the port set in the .env file
